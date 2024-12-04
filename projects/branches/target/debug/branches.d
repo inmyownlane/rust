f551d9a76021e6aa6a42590289e387e2adb300c3
dima <inmyownlanefr@proton.me>
2024-12-04 12:23:14 +0000

    chapter 3

---
+++ projects/branches/target/debug/branches.d
@@ -0,0 +1,1 @@
+/Users/dmitrijsivanovs/Documents/Developer/rust/projects/branches/target/debug/branches: /Users/dmitrijsivanovs/Documents/Developer/rust/projects/branches/src/main.rs
---
+++ projects/branches/target/debug/deps/branches-b68b71a2ebaf0672.d
@@ -0,0 +1,5 @@
+/Users/dmitrijsivanovs/Documents/Developer/rust/projects/branches/target/debug/deps/branches-b68b71a2ebaf0672: src/main.rs
+
+/Users/dmitrijsivanovs/Documents/Developer/rust/projects/branches/target/debug/deps/branches-b68b71a2ebaf0672.d: src/main.rs
+
+src/main.rs:
